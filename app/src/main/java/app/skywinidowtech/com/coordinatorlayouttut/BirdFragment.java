929adb8afa188ffb2e1151f42962bd062e7d97cd
pathakrohit08 <rohit.arun.pathak@M5-D-58048R1.dir.svc.accenture.com>
2016-02-12 20:25:45 +0530

    Initial commit..This is the first commit from my end

---
+++ app/src/main/java/app/skywinidowtech/com/coordinatorlayouttut/BirdFragment.java
@@ -0,0 +1,28 @@
+package app.skywinidowtech.com.coordinatorlayouttut;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rohit.arun.pathak on 2/12/2016.
+ */
+public class BirdFragment  extends Fragment {
+
+    public BirdFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.bird_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/app/skywinidowtech/com/coordinatorlayouttut/DogFragment.java
@@ -0,0 +1,29 @@
+package app.skywinidowtech.com.coordinatorlayouttut;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rohit.arun.pathak on 2/12/2016.
+ */
+public class DogFragment extends Fragment {
+
+
+    public DogFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.dog_fragment, container, false);
+    }
+}
